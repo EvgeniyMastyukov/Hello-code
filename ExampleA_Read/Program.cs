e31bc9505273265ddb09e56fe5b4fefaa11d7c3e
Mastyukov Evgeniy <e.mastyukov@mail.ru>
2022-05-20 17:37:41 +0300

    Add Example A Read

---
+++ ExampleA_Read/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Нажмите любую клавишу, а затем Enter");
+int key1 = Console.Read();
+Console.WriteLine($"Код нажатой клавиши: {key1}");
+Console.WriteLine("Символьное предвставление: " + Convert.ToChar(key1));
